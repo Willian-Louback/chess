924b8f886ed785460bca5938c1ca69f1ae5b82ec
Willian-Louback <williandlouback@gmail.com>
2024-01-10 21:59:19 -0300

    teste

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,11 @@
 import express from "express";
 import cors from "cors";
-// import { WebSocketServer } from "ws";
-import connectWebSocket from "./config/ws.js";
+// import connectWebSocket from "./config/ws.js";
 import "dotenv/config";
+
+import { WebSocketServer } from "ws";
+import verifyNewPlayer from "./utils/checkers/verifyNewPlayer.js";
+import verifyPlayerColor from "./utils/checkers/verifyPlayerColor.js";
 
 const app = express();
 app.use(cors);
@@ -13,4 +16,52 @@ const server = app.listen(port, () => {
     console.log(`Servidor conectado na porta: ${port}`);
 });
 
-connectWebSocket(server);
+const wss = new WebSocketServer({ server });
+const users = [];
+
+wss.on("connection", (ws) => {
+    console.log("Usuário conectado");
+
+    ws.on("message", (data) => {
+        data = JSON.parse(data);
+        console.log("new message:", data);
+
+        if(verifyNewPlayer(data)) {
+            const isWhite = verifyPlayerColor(users);
+
+            if(isWhite) {
+                users.push({
+                    color: "white",
+                    id: data[1],
+                    socket: ws
+                });
+            } else {
+                users.push({
+                    color: "black",
+                    id: data[1],
+                    socket: ws
+                });
+            }
+
+            ws.send(users[users.length - 1].color);
+        } else { // Só para testar, depois eu arrumo isso
+            users.forEach((user) => {
+                if(user.socket != ws) {
+                    user.socket.send(JSON.stringify(data));
+                    return;
+                }
+            });
+
+        }
+    });
+
+    ws.on("close", () => {
+        users.forEach((user, index) => { // Por enquanto o jogador não vai conseguir relogar caso a internet caia ou algo do tipo
+            if(user.socket == ws) {
+                console.log("user desconectado", user.id);
+                users.splice(index, 1);
+                return;
+            }
+        });
+    });
+});
